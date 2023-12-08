d1ec1e13533696bdf51c2c5664e1a632f5764d1e
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-08 15:38:54 -0500

    Add numpy library and convert input list into 3x3 numpy array in calculate().

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,4 +1,8 @@
+import numpy as np
+
 def calculate(numbers):
     # Check if the input list has exactly 9 numbers
     if len(numbers) != 9:
         raise ValueError("List must contain nine numbers.")
+    # Convert the list to a 3x3 Numpy array
+    matrix = np.array(numbers).reshape(3, 3)
